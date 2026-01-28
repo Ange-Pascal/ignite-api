21fae38d4e74834f77b3dbb6ccf341e169262991
Ange-Pascal <angeakoumoua@gmail.com>
2026-01-28 18:51:23 +0000

    entrypoint file modifié

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,24 +4,18 @@ set -e
 : "${PORT:=8080}"
 
 echo "PORT=${PORT}"
-echo "Waiting for database..."
-python manage.py wait_for_db
 
-if [ "$RUN_MIGRATIONS" = "true" ]; then
-  echo "Applying migrations..."
-  python manage.py migrate --noinput
-fi
+echo "Applying migrations..."
+python manage.py migrate --noinput || true
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput || true
 
 echo "Starting Gunicorn..."
 exec gunicorn app.wsgi:application \
-  --bind "0.0.0.0:${PORT}" \
+  --bind 0.0.0.0:${PORT} \
   --workers 2 \
   --threads 4 \
-  --timeout 120 \
-  --graceful-timeout 30 \
-  --keep-alive 5 \
+  --timeout 0 \
   --access-logfile - \
   --error-logfile -
