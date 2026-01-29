0f9be276251904ce95f68aeeea16ffbf0e61de7c
Ange-Pascal <angeakoumoua@gmail.com>
2026-01-29 17:35:05 +0000

    Test CI/CD

--- app/core/views.py
+++ app/core/views.py
@@ -3,5 +3,5 @@ from django.http import JsonResponse
 def home(request):
     return JsonResponse({
         "status": "success",
-        "message": "API is running 🚀 - version 1"
+        "message": "API is running 🚀 - version 2"
     })
